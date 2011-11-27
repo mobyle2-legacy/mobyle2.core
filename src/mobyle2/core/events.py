5ced28a70597232fa0aba084b634ee2fa73ba966
kiorky <kiorky@cryptelium.net>
2011-11-27 23:53:08 +0100

    handle event

--- src/mobyle2/core/events.py
+++ src/mobyle2/core/events.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 from zope.interface import implements, Interface
+
 class IRegenerateVelruseConfigEvent(Interface):
     """Marker interface"""
 
 class RegenerateVelruseConfigEvent(object):
     implements(IRegenerateVelruseConfigEvent)
-    def __init__(self, registry):
-        self.registry = registry 
+    def __init__(self, request):
+        self.request = request
 
 # vim:set et sts=4 ts=4 tw=0:
